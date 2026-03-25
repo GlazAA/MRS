d7e5de0211a1137b799fb9289a424929abbcf694
GlazAA <ag9265227733@gmail.com>
2026-03-25 09:07:19 +0300

    реализовано: 1. соз кл, но без сценария для мираторга, 2.гл.страница, 3. управление листами 2.1.

--- src/MRS.Infrastructure/obj/Debug/net9.0/MRS.Infrastructure.AssemblyInfo.cs
+++ src/MRS.Infrastructure/obj/Debug/net9.0/MRS.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a8c7b5c1f3a4b1a23795916abb2e84a9aa3101")]
 [assembly: System.Reflection.AssemblyProductAttribute("MRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MRS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
